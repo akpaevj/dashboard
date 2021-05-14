b6902f7440bae46fcf2a73eb40603fd147ce2971
Акпаев Евгений Александрович <akpaevj@gmail.com>
2021-05-14 23:25:13 +0300

    version has been up

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :dashboard do
   name 'Dashboard'
   author 'Акпаев Е.А.'
   description 'Панель статусов с отображением задач в колонках'
-  version '0.0.1'
+  version '0.0.2'
   url ''
   author_url 'https://github.com/akpaevj'
   menu :top_menu, :dashboard, { controller: 'dashboard', action: 'index' }, caption: 'Панель задач', first: true
