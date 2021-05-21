68b8a6131c718eff00c1a36c8dff1dade070709a
Акпаев Евгений Александрович <akpaevj@gmail.com>
2021-05-21 23:34:34 +0300

    color generator algorithm have been changed

--- assets/javascripts/settings.js
+++ assets/javascripts/settings.js
@@ -1,22 +1,43 @@
 function hslToHex(h, s, l) {
-    l /= 100;
-    const a = s * Math.min(l, 1 - l) / 100;
-    const f = n => {
-      const k = (n + h / 30) % 12;
-      const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
-      return Math.round(255 * color).toString(16).padStart(2, '0');
-    };
-    return `#${f(0)}${f(8)}${f(4)}`;
-  }
+  l /= 100;
+  const a = s * Math.min(l, 1 - l) / 100;
+  const f = n => {
+    const k = (n + h / 30) % 12;
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    return Math.round(255 * color).toString(16).padStart(2, '0');
+  };
+  return `#${f(0)}${f(8)}${f(4)}`;
+}
 
-function getRandomColor() {
-    return hslToHex(Math.random() * 360, 100, 45);
+function genereateStatusesColors() {
+  const items = document.querySelectorAll('input[name^="settings[status_color"]');
+  const degreePerItem = 360 / items.length;
+  let currentDegree = 0;
+
+  items.forEach(function(item) {
+    if (currentDegree > 360) {
+      currentDegree = 360;
+    }
+    item.value = hslToHex(currentDegree, 100, 50);
+    currentDegree += degreePerItem;
+  }); 
+}
+
+function genereateProjectColors() {
+  const items = document.querySelectorAll('input[name^="settings[project_color"]');
+  const degreePerItem = 360 / items.length;
+  let currentDegree = 0;
+
+  items.forEach(function(item) {
+    if (currentDegree > 360) {
+      currentDegree = 360;
+    }
+    item.value = hslToHex(currentDegree, 100, 45);
+    currentDegree += degreePerItem;
+  }); 
 }
 
 function generateColors() {
-    document.querySelectorAll('input[type=color]').forEach(function(item) {
-      if (item.value == '#000000') {
-        item.value = getRandomColor();
-      }
-    });
+  genereateStatusesColors();
+  genereateProjectColors();
 }
